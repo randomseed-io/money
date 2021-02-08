ec701dd346b8f12e862f728d80e905073c5ae6be
Paweł Wilk <siefca@gnu.org>
2021-02-08 10:56:58 +0100

    Removed Java version dependent formatting test

--- test/randomseed/money/format_test.clj
+++ test/randomseed/money/format_test.clj
@@ -9,10 +9,7 @@
 
 (deftest test-formatting-with-default-formatter-and-provided-locale
   (are [formatted money] (is (= formatted (fmt/format money (Locale/UK))))
-       "€10.00"   (ams/amount-of cu/EUR 10.00)
-       "£10.00"   (ams/amount-of cu/GBP 10.00)
-       "US$10.00" (ams/amount-of cu/USD 10.00))
+       "£10.00"   (ams/amount-of cu/GBP 10.00))
   (are [formatted money] (is (= formatted (fmt/format money (l/locale :pl))))
-       "€10,00"   (ams/amount-of cu/EUR 10.00)
        "GBP10,00" (ams/amount-of cu/GBP 10.00)
        "USD10,00" (ams/amount-of cu/USD 10.00)))
